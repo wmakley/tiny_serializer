fc86aa35a4c3452266f5b010643eb8fdc138cb8f
William Makley <wmakley@tortus.com>
2018-07-12 16:00:56 -0400

    document RailsExtensions

--- lib/simple_serializer/rails_extensions.rb
+++ lib/simple_serializer/rails_extensions.rb
@@ -1,9 +1,11 @@
 class SimpleSerializer
   module RailsExtensions
+    # Alias of Rails.application.routes.url_helpers
     def url_helpers
       Rails.application.routes.url_helpers
     end
 
+    # Make Rails.logger the default logger.
     def logger
       super || Rails.logger
     end
